bfa32989e1aa5f9883378eba3fd4321c5171db1c
Esmaeil Emami <esmaeilemami84@gmail.com>
2020-12-30 15:13:36 +0330

    add Context

---
+++ NetLine/NetLine.Infrastructure/Context/DependencyInjection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetLine.Infrastructure.Context;
+
+namespace Eshop.Infrastructure.Context
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region Db Context
+
+            services.AddDbContext<NetLineDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+
+            #endregion
+
+            #region Identity
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+                {
+                    options.User.RequireUniqueEmail = true;
+                    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+                })
+                .AddEntityFrameworkStores<NetLineDbContext>()
+                .AddDefaultTokenProviders();
+
+            #endregion
+
+            #region IoC
+
+            /*services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+            services.AddScoped<IAdminRepository, AdminRepository>();
+
+            services.AddScoped<IManageAccountRepository, ManageAccountRepository>();*/
+
+            #endregion
+
+            services.AddAuthentication();
+
+            return services;
+        }
+    }
+}
---
+++ NetLine/NetLine.Infrastructure/Context/NetLineDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NetLine.Infrastructure.Context
+{
+    public class NetLineDbContext : IdentityDbContext
+    {
+        public NetLineDbContext(DbContextOptions<NetLineDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
