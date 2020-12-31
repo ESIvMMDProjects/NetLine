5bc15160a00880eef81752a0d637916759d634c8
Noname R <noname1381@outlook.com>
2021-01-01 00:26:00 +0330

    add Register and Login

---
+++ NetLine/NetLine.Domain/ViewModels/Account/User/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLine.Domain.ViewModels.Account.User
+{
+   public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "نام کاربری")]
+        public string Username { get; set; }
+        [Required]
+        [Display(Name = "کلمه ی عبور")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "مرا به خاطر بسپار")]
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ NetLine/NetLine.Domain/ViewModels/Account/User/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLine.Domain.ViewModels.Account.User
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "ایمیل")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "نام کاربری")]
+        public string UserName { get; set; }
+        [Required]
+        [Display(Name = "کلمه ی عبور")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [Display(Name = "تکرار کلمه ی عبور ")]
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
