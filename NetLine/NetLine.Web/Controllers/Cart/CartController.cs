24d478eac64724fabb637c7d666d8423a3270ebc
Noname R <noname1381@outlook.com>
2021-01-04 23:48:54 +0330

    Fix Controller

---
+++ NetLine/NetLine.Web/Controllers/Cart/CartController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NetLine.Domain.Services.UnitOfWork;
+
+namespace NetLine.Web.Controllers.Cart
+{
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IActionResult> AddToCart(int itemId , string userId)
+        {
+          await  _unitOfWork.CartRe.AddToCart(itemId, userId);
+          return RedirectToAction("index", "Home");
+        }
+    }
+    }
+
+
